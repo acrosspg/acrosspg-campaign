fc423e83f009f98d4accadf65d1ea3be09a5710d
acrosspg@gmail.com <acrosspg@gmail.com@587fe873-5d56-ecd0-81db-b2b73e592566>
2014-01-07 19:46:45 +0000

    commas

--- sidebar.js
+++ sidebar.js
@@ -3,12 +3,12 @@ index = 0;
 
 images[0] = "<a href = 'http://www.flipkart.com/dnb-cet-review-primary-post-diploma-volume-1-2-3rd/p/itmdmfu2wk2gzwpc?pid=9788192022420&affid=acrosspgya' target='_blank'><img src='http://tiny.cc/ur7l6w' alt='DNB CET Review 3rr Edition'></a>";
 images[1] = "<a href = 'http://www.flipkart.com/roams-review-all-medical-subjects-10th/p/itmdnnanqtjdsatt?pid=9788189926793&affid=acrosspgya' target='_blank'><img src='https://lh3.googleusercontent.com/-Ldb__mwvXeE/UoP_FSZVe9I/AAAAAAAABCQ/wXtuINWb3s8/w300-h250-no/roams-10ed.jpg' alt='ROAMS 10th Edition'></a>";
-images[2] = "<iframe src="http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138912321680994255" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0" ></iframe>";
+images[2] = "<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138912321680994255' style='width:120px;height:240px;' scrolling='no' marginwidth='0' marginheight='0' frameborder='0' ></iframe>";
 images[3] = "<a href = 'http://www.flipkart.com/dnb-cet-review-primary-post-diploma-2013-supplement-1st/p/itmdqd7arbhw6qux?pid=9788189477202&affid=acrosspgya' target='_blank'><img src='https://lh4.googleusercontent.com/s5gLD3gZMWd7IdVaOJdcGRGZeIg9H3KZ0yof5Y1-EJg=w300-h250-no' alt='DNB CET Review 2013 Supplement'></a>";
 images[4] = "<a href = 'http://www.flipkart.com/sure-success-magic-7th/p/itmdq4677xzksya5?pid=9789351520009&&affid=acrosspgya' target='_blank'><img src='https://lh5.googleusercontent.com/-qr-8BSmpVCE/UoTOhctYQYI/AAAAAAAABCw/HcSl1UnrY8Y/w300-h250-no/sure+success+magic+7ed.jpg' alt='Sure Success Magic'></a>";
-images[5] = "<iframe src="http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138098786985985528" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0" ></iframe>";
-images[6] = "<iframe src="http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138912312179324023" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0" ></iframe>";
-images[7] = "<iframe src="http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138912334559088353" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0" ></iframe>";
+images[5] = "<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138098786985985528' style='width:120px;height:240px;' scrolling='no' marginwidth='0' marginheight='0' frameborder='0' ></iframe>";
+images[6] = "<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138912312179324023' style='width:120px;height:240px;' scrolling='no' marginwidth='0' marginheight='0' frameborder='0' ></iframe>";
+images[7] = "<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138912334559088353' style='width:120px;height:240px;' scrolling='no' marginwidth='0' marginheight='0' frameborder='0' ></iframe>";
 
 
 index = Math.floor(Math.random() * images.length);
