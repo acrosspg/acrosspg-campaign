3f826e1a54d749cd1af55d792bf3a77f94d1d61d
acrosspg@gmail.com <acrosspg@gmail.com@587fe873-5d56-ecd0-81db-b2b73e592566>
2014-03-20 21:33:07 +0000

    remove tiny.cc

--- sidebar.js
+++ sidebar.js
@@ -5,11 +5,11 @@ images[0] = "<a href = 'http://www.flipkart.com/dnb-cet-review-primary-post-dipl
 images[1] = "<a href = 'http://www.flipkart.com/roams-review-all-medical-subjects-10th/p/itmdnnanqtjdsatt?pid=9788189926793&affid=acrosspgya' target='_blank'><img src='https://lh3.googleusercontent.com/-Ldb__mwvXeE/UoP_FSZVe9I/AAAAAAAABCQ/wXtuINWb3s8/w300-h250-no/roams-10ed.jpg' alt='ROAMS 10th Edition'></a>";
 images[2] = "<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138912321680994255' style='width:120px;height:240px;' scrolling='no' marginwidth='0' marginheight='0' frameborder='1' ></iframe>";
 images[3] = "<a href = 'http://www.flipkart.com/dnb-cet-review-primary-post-diploma-2013-supplement-1st/p/itmdqd7arbhw6qux?pid=9788189477202&affid=acrosspgya' target='_blank'><img src='https://lh4.googleusercontent.com/s5gLD3gZMWd7IdVaOJdcGRGZeIg9H3KZ0yof5Y1-EJg=w300-h250-no' alt='DNB CET Review 2013 Supplement'></a>";
-images[4] = "<a href = 'http://tiny.cc/motox-acrosspg' target='_blank'><img src='http://tiny.cc/99c1cx' alt='Moto X'></a>";
+images[4] = "<a href = 'http://www.flipkart.com/motorola/motox?affid=acrosspgya' target='_blank'><img src='https://lh5.googleusercontent.com/-KyC9M5mQ9zU/UytE4oife_I/AAAAAAAABXs/vAXltqaOHf0/s300/Moto-X-Flipkart.jpg' alt='Moto X'></a>";
 images[5] = "<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138098786985985528' style='width:120px;height:240px;' scrolling='no' marginwidth='0' marginheight='0' frameborder='1' ></iframe>";
 images[6] = "<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138912312179324023' style='width:120px;height:240px;' scrolling='no' marginwidth='0' marginheight='0' frameborder='1' ></iframe>";
 images[7] = "<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138912334559088353' style='width:120px;height:240px;' scrolling='no' marginwidth='0' marginheight='0' frameborder='1' ></iframe>";
-images[8] = "<a href = 'http://tiny.cc/motog-acrosspg' target='_blank'><img src='http://tiny.cc/gfd1cx' alt='Moto G'></a>";
+images[8] = "<a href = 'http://www.flipkart.com/motorola/motog?affid=acrosspgya' target='_blank'><img src='https://lh3.googleusercontent.com/-MTRyZz3Lwgc/UytE42Ms8lI/AAAAAAAABXw/J2ffyLIKFXo/s300/moto%2520g.jpg' alt='Moto G'></a>";
 
 index = Math.floor(Math.random() * images.length);
 document.write(images[index]);
