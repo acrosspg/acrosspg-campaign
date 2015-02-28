94a1bc42b0de7a412f52fdc8759de027dc28d2ff
acrosspg@gmail.com <acrosspg@gmail.com@587fe873-5d56-ecd0-81db-b2b73e592566>
2015-02-28 16:37:33 +0000

    target='_blank'

--- inline ads.js
+++ inline ads.js
@@ -2,16 +2,16 @@ var images = [],
 index = 0;
 
 images[0] = "<a href = 'http://dl.flipkart.com/dl/essentials-medical-pharmacology-english-7th/p/itmdmpfrhwazhfag?pid=9789350259375&affid=acrosspgya'>Essentials of Medical Pharmacology-7th Edition by KD Tripathi</a>";
-images[1] = "<a href = 'http://fkrt.it/NLsB6LNN'>LMR LAST MINUTE REVISION SERIES FOR NBE/AIPGMEE/FMGE - Latest Edition</a>";
-images[2] = "<a href = 'http://fkrt.it/u2YhlrNN'>Buy Platinum Notes - Preclinical Sciences Volume 1 (2014 - 15) 4th Edition</a>";
-images[3] = "<a href = 'http://axpg.ml/18gZLJK'>Buy Pharmacology : Prep Manual for Undergraduates 2nd Edition</a>";
-images[4] = "<a href = 'http://axpg.ml/1zc62vy'>Buy Concise Pathology for Exam Preparation by Geetika Khanna Bhattacharya 2nd Edition</a>";
-images[5] = "<a href = 'http://axpg.ml/18h36IN'>Buy Platinum Notes - Preclinical Sciences Volume 1 (2014 - 15) 4th Edition</a>";
-images[6] = "<a href = 'http://axpg.ml/1GnVteE'>Buy Platinum Notes - Medical Sciences Volume 2 (2014 - 15) 4th Edition</a>";
-images[7] = "<a href = 'http://axpg.ml/1wj9v0V'>SMART STUDY SERIES - Obstetrics & Gynecology 3rd Edition</a>";
-images[8] = "<a href = 'http://axpg.ml/1BLzNds'>AIPGMEE : Self Assessment and Review by MUDIT KHANNA (Volume - 1) 2012 - 2007 12th Edition</a>";
-images[9] = "<a href = 'http://bit.ly/rapidreviewpatho'>Rapid Review Pathology (English) 4th Edition</a>";
-images[10] = "<a href = 'http://axpg.ml/1GnVteE'>Buy Platinum Notes - Medical Sciences Volume 2 (2014 - 15) 4th Edition</a>";
+images[1] = "<a href = 'http://fkrt.it/NLsB6LNN' target='_blank'>LMR LAST MINUTE REVISION SERIES FOR NBE/AIPGMEE/FMGE - Latest Edition</a>";
+images[2] = "<a href = 'http://fkrt.it/u2YhlrNN' target='_blank'>Buy Platinum Notes - Preclinical Sciences Volume 1 (2014 - 15) 4th Edition</a>";
+images[3] = "<a href = 'http://axpg.ml/18gZLJK' target='_blank'>Buy Pharmacology : Prep Manual for Undergraduates 2nd Edition</a>";
+images[4] = "<a href = 'http://axpg.ml/1zc62vy' target='_blank'>Buy Concise Pathology for Exam Preparation by Geetika Khanna Bhattacharya 2nd Edition</a>";
+images[5] = "<a href = 'http://axpg.ml/18h36IN' target='_blank'>Buy Platinum Notes - Preclinical Sciences Volume 1 (2014 - 15) 4th Edition</a>";
+images[6] = "<a href = 'http://axpg.ml/1GnVteE' target='_blank'>Buy Platinum Notes - Medical Sciences Volume 2 (2014 - 15) 4th Edition</a>";
+images[7] = "<a href = 'http://axpg.ml/1wj9v0V' target='_blank'>SMART STUDY SERIES - Obstetrics & Gynecology 3rd Edition</a>";
+images[8] = "<a href = 'http://axpg.ml/1BLzNds' target='_blank'>AIPGMEE : Self Assessment and Review by MUDIT KHANNA (Volume - 1) 2012 - 2007 12th Edition</a>";
+images[9] = "<a href = 'http://bit.ly/rapidreviewpatho' target='_blank'>Rapid Review Pathology (English) 4th Edition</a>";
+images[10] = "<a href = 'http://axpg.ml/1GnVteE' target='_blank'>Buy Platinum Notes - Medical Sciences Volume 2 (2014 - 15) 4th Edition</a>";
 
 
 
